3a3847a816fe8a3de9ee3abc1c800c6a9d970b79
Eric Miller <eric.miller@cerner.com>
2019-09-28 15:02:40 -0500

    Update to new release directory

--- Formula/urlstatus.rb
+++ Formula/urlstatus.rb
@@ -1,16 +1,20 @@
 class Urlstatus < Formula
   desc "URL Status Checker"
   homepage "https://github.com/ericem/two-pow-five/tree/master/urlstatus"
-  url "https://github.com/ericem/two-pow-five/raw/master/urlstatus/urlstatus-0.1.0.tar.gz"
-  sha256 "187eb4e367f2533666668de299bf046df5f3a42e81245b2747d17b1ce8f13b32"
+  url "https://github.com/ericem/two-pow-five/raw/master/urlstatus/release/urlstatus-0.1.0.tar.gz"
+  sha256 "f6b67211ad237b09eb1f02493d06a60ceda47218c3a6241715cd33102e24d0a7"
+
+ bottle do
+    root_url "https://github.com/ericem/two-pow-five/raw/master/urlstatus/release/"
+    cellar :any
+    sha256 "c7c54384d7212bf551878cbe86dd04e94139ec67e7bbb39a51f34c700288276e" => :mojave
+  end
 
   depends_on "crystal" => :build
   depends_on "libevent"
 
   def install
-    mkdir bin
-    system "make", "release"
-    cp "./urlstatus", bin
+    system "make", "PREFIX=#{prefix}", "install"
   end
 
 end
