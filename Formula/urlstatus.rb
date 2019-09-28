747c59cd8ad8535fa920e06be73c6d4c86a1989a
Eric Miller <eric.miller@cerner.com>
2019-09-28 16:42:33 -0500

    Correct the root url

--- Formula/urlstatus.rb
+++ Formula/urlstatus.rb
@@ -5,7 +5,7 @@ class Urlstatus < Formula
   sha256 "a1a864533cad4da38b629f374e22408b9d3e608818e077e8d1a78b1a9c916b82"
 
  bottle do
-    root_url "https://github.com/ericem/two-pow-five/raw/master/urlstatus/release/"
+    root_url "https://github.com/ericem/two-pow-five/raw/master/urlstatus/release"
     cellar :any
     sha256 "c6070bdc8f7cd64ebe6d0479b6174bf22b1431a1c2a15e4f195ca5d4713b4e11" => :mojave
   end
