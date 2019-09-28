7226700d704319852ce3eb51eca794ed09f1bd2b
Eric Miller <eric.miller@cerner.com>
2019-09-28 15:21:12 -0500

    Updated formula with correct sha256 sums

--- Formula/urlstatus.rb
+++ Formula/urlstatus.rb
@@ -2,12 +2,12 @@ class Urlstatus < Formula
   desc "URL Status Checker"
   homepage "https://github.com/ericem/two-pow-five/tree/master/urlstatus"
   url "https://github.com/ericem/two-pow-five/raw/master/urlstatus/release/urlstatus-0.1.0.tar.gz"
-  sha256 "f6b67211ad237b09eb1f02493d06a60ceda47218c3a6241715cd33102e24d0a7"
+  sha256 "f477e93fb3d063a38e5c8e7fc9ff75825286ab4be3fbad417c1ec616857199eb"
 
  bottle do
     root_url "https://github.com/ericem/two-pow-five/raw/master/urlstatus/release/"
     cellar :any
-    sha256 "c7c54384d7212bf551878cbe86dd04e94139ec67e7bbb39a51f34c700288276e" => :mojave
+    sha256 "5e66564dc0fe8f062dd1a527072c19d3bf9263d91cff450976b941eff20eb84c" => :mojave
   end
 
   depends_on "crystal" => :build
