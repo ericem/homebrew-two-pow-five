1ebe7f1bd22ea89b8ddebb0e14538ef09df96a74
Eric Miller <eric.miller@cerner.com>
2019-09-30 20:53:03 -0500

    Bump certdump version

--- Formula/certdump.rb
+++ Formula/certdump.rb
@@ -1,13 +1,13 @@
 class Certdump < Formula
   desc "Certificate Dumper"
   homepage "https://github.com/ericem/two-pow-five/tree/master/certdump"
-  url "https://github.com/ericem/two-pow-five/raw/master/certdump/release/certdump-0.2.0.tar.gz"
-  sha256 "8ad091d34203f58ca76be6ed20cbda36394fb1013b9e93c6137402cd4538be31"
+  url "https://github.com/ericem/two-pow-five/raw/master/certdump/release/certdump-0.3.0.tar.gz"
+  sha256 "fee3f7370ebeeac726171b812f6e9a17daf5f010593c2f67abbc069971c2a7fe"
 
  bottle do
     root_url "https://github.com/ericem/two-pow-five/raw/master/certdump/release"
     cellar :any
-    sha256 "b4b177f4b22e2cdf92f1d7bd1def4b4e4b4b98025076bab45759023211ec4e74" => :mojave
+    sha256 "a046d2f83c31d3ffa1971e077b388bd35da1dac8f323de7f350d70eae2a2189a" => :mojave
   end
 
   depends_on "crystal" => :build
