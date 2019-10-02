e803279de7631cf2feb369503c967a614bfdbf09
Eric Miller <eric.miller@cerner.com>
2019-10-01 23:25:10 -0500

    Fix sha256 sums

--- Formula/serveme.rb
+++ Formula/serveme.rb
@@ -2,12 +2,12 @@ class Serveme < Formula
   desc "Super Simple Static File Server"
   homepage "https://github.com/ericem/two-pow-five/tree/master/serveme"
   url "https://github.com/ericem/two-pow-five/raw/master/serveme/release/serveme-0.1.0.tar.gz"
-  sha256 "6173aa68c4410e161ebc9a0f3665f0f25c882caea50113ae586e94a0ff523134"
+  sha256 "1402091e632b10a6f0d63697126d1941bf55e2dd36a8664f8e7051b5e1a0af6f"
 
  bottle do
     root_url "https://github.com/ericem/two-pow-five/raw/master/serveme/release"
     cellar :any
-    sha256 "af345f2d2e330d3e18461acc57c328c063437c16bf48c6cae00534cd738c6e6b" => :mojave
+    sha256 "2d117de14cab43d76290b66d3648f0f6f81fff47fa3325dc79a688c9fb8538c6" => :mojave
   end
 
   depends_on "crystal" => :build
