31c878e1bf3b2fe912a810a3ab1d1d2444580e53
Eric Miller <eric.miller@cerner.com>
2019-10-04 18:05:34 -0500

    Add formula for speedtest

--- Formula/speedtest.rb
+++ Formula/speedtest.rb
@@ -1,7 +1,7 @@
 class Speedtest < Formula
   desc "Certificate Dumper"
   homepage "https://github.com/ericem/two-pow-five/tree/master/speedtest"
-  url "https://github.com/ericem/two-pow-five/raw/master/speedtest/release/speedtest-0.3.0.tar.gz"
+  url "https://github.com/ericem/two-pow-five/raw/master/speedtest/release/speedtest-0.1.0.tar.gz"
   sha256 "9bf11868881788666f1d11776d5624a9453f15403c65a29cadcb57a5fb633b9a"
 
  bottle do
