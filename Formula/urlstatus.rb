47184307b261787751f326cbefb159041bc96dec
Eric Miller <eric.miller@cerner.com>
2019-10-14 21:27:26 -0500

    Bump version for new release of urlstatus

--- Formula/urlstatus.rb
+++ Formula/urlstatus.rb
@@ -1,13 +1,13 @@
 class Urlstatus < Formula
   desc "URL Status Checker"
   homepage "https://github.com/ericem/two-pow-five/tree/master/urlstatus"
-  url "https://github.com/ericem/two-pow-five/raw/master/urlstatus/release/urlstatus-0.1.0.tar.gz"
-  sha256 "a1a864533cad4da38b629f374e22408b9d3e608818e077e8d1a78b1a9c916b82"
+  url "https://github.com/ericem/two-pow-five/raw/master/urlstatus/release/urlstatus-0.1.1.tar.gz"
+  sha256 "5a9a713cb4d116ccaaf42921c230ae040f6efff939cb64f99c921fbb2a0d745d"
 
  bottle do
     root_url "https://github.com/ericem/two-pow-five/raw/master/urlstatus/release"
     cellar :any
-    sha256 "c6070bdc8f7cd64ebe6d0479b6174bf22b1431a1c2a15e4f195ca5d4713b4e11" => :mojave
+    sha256 "8a039bdba3cd43a71b050eaabe3a1db244fba3f22270f5fc5eda543139ff6b5b" => :mojave
   end
 
   depends_on "crystal" => :build
