881e3e72a9be1c66fb6be65fe974b12ff6ee61e3
Eric Miller <eric.miller@cerner.com>
2019-10-14 21:17:37 -0500

    Bump healthstatus to for new version

--- Formula/healthstatus.rb
+++ Formula/healthstatus.rb
@@ -2,12 +2,12 @@ class Healthstatus < Formula
   desc "Process Health Status"
   homepage "https://github.com/ericem/two-pow-five/tree/master/healthstatus"
   url "https://github.com/ericem/two-pow-five/raw/master/healthstatus/release/healthstatus-0.1.0.tar.gz"
-  sha256 "34349f9a0f648dcfee596b2a70eaf82d3c79787ec633de642fd552f4a6e0514f"
+  sha256 "3048b15fb81a01110d6356e628447f2ed2a97c16b64e9af7f0ff8391737fb91e"
 
  bottle do
     root_url "https://github.com/ericem/two-pow-five/raw/master/healthstatus/release"
     cellar :any
-    sha256 "e9912f3ed8acb7064fc5363301b4d6184f1cd855b413b1792c0088bbbf010d6c" => :mojave
+    sha256 "71ec5042bdf1b84e7993ef793c5f827b2bddb2e9df7daf8827009efd1e117beb" => :mojave
   end
 
   depends_on "crystal" => :build
